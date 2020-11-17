78efb773b7df2a4e6473ee5cc1cb3b9931d8538e
Wissa Michael <wissa@neugelb.com>
2020-11-18 00:47:25 +0100

    Initial Commit

---
+++ Cash Split/Cash_SplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cash_SplitApp.swift
+//  Cash Split
+//
+//  Created by Wissa Michael on 18.11.20.
+//
+
+import SwiftUI
+
+@main
+struct Cash_SplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
